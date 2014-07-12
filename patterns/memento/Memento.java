3961f1852cdde37152a25fcb8c917c2ecc70ed49
levis <levisjani@rocketmail.com>
2014-07-13 01:21:59 +0200

    Create Memento.java

---
+++ patterns/memento/Memento.java
@@ -0,0 +1,16 @@
+/**
+*
+* @author levisjani
+*/
+
+public class Memento {
+   private String state;
+
+   public Memento(String state){
+      this.state = state;
+   }
+
+   public String getState(){
+      return state;
+   }	
+}
