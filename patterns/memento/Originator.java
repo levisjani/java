e05795c0d278bf3a2eb192de67838f30b00672e0
levis <levisjani@rocketmail.com>
2014-07-13 01:22:56 +0200

    Create Originator.java

---
+++ patterns/memento/Originator.java
@@ -0,0 +1,25 @@
+/**
+*
+* @author levisjani
+*/
+
+public class Originator {
+   private String state;
+
+   public void setState(String state){
+      this.state = state;
+   }
+
+   public String getState(){
+      return state;
+   }
+
+   public Memento saveStateToMemento(){
+      return new Memento(state);
+   }
+
+   public void getStateFromMemento(Memento memento){
+       if (memento!=null)
+        state = memento.getState();
+   }
+}
