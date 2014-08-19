98d7649d4a0906aa7600b031da47cdccfb54364d
levis <levisjani@rocketmail.com>
2014-08-20 00:16:53 +0200

    Create book.java

---
+++ bank_app/ejb/book.java
@@ -0,0 +1,15 @@
+//Create a model class
+
+public class Book implements Serializable{
+    
+   private int id;
+   private String name;
+
+   public Book(){        
+   }
+   
+   public int getId() {
+      return id;
+   }
+   ...
+}
