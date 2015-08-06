e2dc9febe6703379366ad6b420c97d121a8dd64e
Levis Jani <levisjani@rocketmail.com>
2015-08-06 22:20:16 +0200

    Update Classification.java

--- oozinoz/fireworks/Classification.java
+++ oozinoz/fireworks/Classification.java
@@ -1,25 +1,5 @@
 package com.oozinoz.fireworks;
 
-/*
- * Copyright (c) 2001 Steven J. Metsker.
- * 
- * Steve Metsker makes no representations or warranties about
- * the fitness of this software for any particular purpose, 
- * including the implied warranty of merchantability.
- *
- * Please use this software as you wish with the sole
- * restriction that you may not claim that you wrote it.
- */
-/**
- * Objects of this class represent classifications of 
- * fireworks. 
- *
- * This class defines the data type of constants that the
- * ClassificationConstants interface defines.
- *
- * @author Steven J. Metsker
- *
- */
 public class Classification 
 {
 /**
