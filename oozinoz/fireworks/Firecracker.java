3caae7f293d8b3d8da5dd9f8dc58dd7d5dbf2267
Levis Jani <levisjani@rocketmail.com>
2015-08-06 22:21:16 +0200

    Update Firecracker.java

--- oozinoz/fireworks/Firecracker.java
+++ oozinoz/fireworks/Firecracker.java
@@ -1,23 +1,5 @@
 package com.oozinoz.fireworks;
 
-/*
- * Copyright (c) 2001 Steven J. Metsker.
- * 
- * Steve Metsker makes no representations or warranties about
- * the fitness of this software for any particular purpose, 
- * including the implied warranty of merchantability.
- *
- * Please use this software as you wish with the sole
- * restriction that you may not claim that you wrote it.
- */
-/**
- * A firework that just explodes, a behavior that many
- * people find quite entertaining in itself.
- * 
- * @author Steven J. Metsker
- *
- * @version 1.0
- */
 public class Firecracker extends Firework 
 {
 /**
