4ad5446062e773d7f3284d853c14084c6cea7b12
Levis Jani <levisjani@rocketmail.com>
2015-08-06 22:19:39 +0200

    Create Classification.java

---
+++ oozinoz/fireworks/Classification.java
@@ -0,0 +1,34 @@
+package com.oozinoz.fireworks;
+
+/*
+ * Copyright (c) 2001 Steven J. Metsker.
+ * 
+ * Steve Metsker makes no representations or warranties about
+ * the fitness of this software for any particular purpose, 
+ * including the implied warranty of merchantability.
+ *
+ * Please use this software as you wish with the sole
+ * restriction that you may not claim that you wrote it.
+ */
+/**
+ * Objects of this class represent classifications of 
+ * fireworks. 
+ *
+ * This class defines the data type of constants that the
+ * ClassificationConstants interface defines.
+ *
+ * @author Steven J. Metsker
+ *
+ */
+public class Classification 
+{
+/**
+ * The role of this class is to enumerate valid firework 
+ * classifications. This class should not be instantiated
+ * except for the static variables it creates itself, so
+ * this method is protected.
+ */
+protected Classification()
+{
+}
+}
