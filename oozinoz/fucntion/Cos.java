21fb3020262afb0de1da293ffdefbedb0627192d
Levis Jani <levisjani@rocketmail.com>
2015-08-09 13:52:19 +0200

    Update Cos.java

--- oozinoz/fucntion/Cos.java
+++ oozinoz/fucntion/Cos.java
@@ -1,27 +1,9 @@
 package com.oozinoz.function;
 
-/*
- * Copyright (c) 2001 Steven J. Metsker.
- * 
- * Steve Metsker makes no representations or warranties about
- * the fitness of this software for any particular purpose, 
- * including the implied warranty of merchantability.
- *
- * Please use this software as you wish with the sole
- * restriction that you may not claim that you wrote it.
- */
-/**
- * Wrap the Math.cos() function around a given source.
- * 
- * @author Steven J. Metsker
- */
 public class Cos extends Function 
 {
 
 /**
- * Construct a cosine function that decorates the 
- * provided source function.
- *
  * @param Function the source function that this function
  *                 wraps
  */
@@ -32,10 +14,6 @@ public Cos(Function f)
 /**
  * Return Math.cos() applied to the source function's value
  * at time t.
- *
- * @param t the time function that goes 0 to 1 and that
- *          other functions use as a parameter
- *
  * @return Math.cos() applied to the source function's value
  *         at time t.
  */
