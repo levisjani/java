409f07ca7fec5a5c96ab260e666a64cd77975bab
Levis Jani <levisjani@rocketmail.com>
2015-08-09 13:48:47 +0200

    Update Arithmetic.java

--- oozinoz/fucntion/Arithmetic.java
+++ oozinoz/fucntion/Arithmetic.java
@@ -3,17 +3,7 @@ package com.oozinoz.function;
 public class Arithmetic extends Function 
 {
 	protected char operator;
-/**
- * Construct an arithmetic function that decorates the 
- * provided source functions.
- *
- * @param char the operator to apply (+ - * /)
- *
- * @param Function the first source function that this
- *                 function wraps
- *
- * @param Function the second source function 
- */
+
 public Arithmetic(
 	char operator,
 	Function f1,
@@ -22,18 +12,7 @@ public Arithmetic(
 	super(new Function[] { f1, f2 });
 	this.operator = operator;
 }
-/**
- * Return the result of applying this objects' arithmetic 
- * operator (+ - * /) to the source functions' values
- * at time t.
- *
- * @param t the time function that goes 0 to 1 and that
- *          other functions use as a parameter
- *
- * @return the result of applying this objects' arithmetic 
- *         operator (+ - * /) to the source functions' values
- *         at time t
- */
+
 public double f(double t)
 {
 	switch (operator)
