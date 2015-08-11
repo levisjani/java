6a4bfb11a343067a94b119ba799c8db546f2f65c
Levis Jani <levisjani@rocketmail.com>
2015-08-11 22:43:37 +0200

    Create ShellController.java

---
+++ oozinoz/controll/ShellController.java
@@ -0,0 +1,44 @@
+package com.oozinoz.control;
+
+/*
+ * Copyright (c) 2001 Steven J. Metsker.
+ * 
+ * Steve Metsker makes no representations or warranties about
+ * the fitness of this software for any particular purpose, 
+ * including the implied warranty of merchantability.
+ *
+ * Please use this software as you wish with the sole
+ * restriction that you may not claim that you wrote it.
+ */
+/**
+ * This class is just here to show help show an abstraction 
+ * that is not (yet) a bridge. See the "Bridge" chapter in
+ * "The Design Patterns Java Workbook."
+ *
+ * @author Steven J. Metsker
+ *
+ */
+public class ShellController extends MachineController_1 
+{
+/**
+ * Return the maximum size of this machine's input queue.
+ *
+ * @return the maximum size of this machine's input queue
+ */
+public int getQueueMax()
+{
+	return 10;
+}
+/**
+ * Start this machine; not actually implemented.
+ */
+public void start()
+{
+}
+/**
+ * Stop this machine; not actually implemented.
+ */
+public void stop()
+{
+}
+}
