357ae5ec01cccd028e5b9a8d7fb261bce526578b
Levis Jani <levisjani@rocketmail.com>
2015-08-12 21:55:41 +0200

    Update engineer.java

--- oozinoz/machine/engineer.java
+++ oozinoz/machine/engineer.java
@@ -1,24 +1,5 @@
 package com.oozinoz.machine;
 
-/*
- * Copyright (c) 2001 Steven J. Metsker.
- * 
- * Steve Metsker makes no representations or warranties about
- * the fitness of this software for any particular purpose, 
- * including the implied warranty of merchantability.
- *
- * Please use this software as you wish with the sole
- * restriction that you may not claim that you wrote it.
- */
- 
-/**
- * This isn't much of a model, but it provides a data type
- * that represents the role of a person that can be
- * responsible for a machine.
- *
- * @author Steven J. Metsker
- *
- */
 public class Engineer 
 {
 	protected int employeeId;
