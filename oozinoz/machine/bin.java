2935544a348402b313ed3d3602c531998f264098
Levis Jani <levisjani@rocketmail.com>
2015-08-12 21:54:16 +0200

    Update bin.java

--- oozinoz/machine/bin.java
+++ oozinoz/machine/bin.java
@@ -1,24 +1,5 @@
 package com.oozinoz.machine;
 
-/*
- * Copyright (c) 2001 Steven J. Metsker.
- * 
- * Steve Metsker makes no representations or warranties about
- * the fitness of this software for any particular purpose, 
- * including the implied warranty of merchantability.
- *
- * Please use this software as you wish with the sole
- * restriction that you may not claim that you wrote it.
- */
- 
-/**
- * This is a minimal model of a "bin," a large plastic
- * rectangular basket that holds fireworks materials as
- * they go through the factory.
- *
- * @author Steven J. Metsker
- *
- */
 public class Bin 
 {
 	protected String id;
