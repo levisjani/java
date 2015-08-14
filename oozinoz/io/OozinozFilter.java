5271c18ac8bd7644189d769e1cc62d04546271fb
Levis Jani <levisjani@rocketmail.com>
2015-08-14 22:02:22 +0200

    Update OozinozFilter.java

--- oozinoz/io/OozinozFilter.java
+++ oozinoz/io/OozinozFilter.java
@@ -2,23 +2,6 @@ package com.oozinoz.io;
 
 import java.io.*;
 
-/*
- * Copyright (c) 2001 Steven J. Metsker.
- * 
- * Steve Metsker makes no representations or warranties about
- * the fitness of this software for any particular purpose, 
- * including the implied warranty of merchantability.
- *
- * Please use this software as you wish with the sole
- * restriction that you may not claim that you wrote it.
- */
-/**
- * This class channels all versions of the write() method to
- * go through the write() method that takes a single 
- * character.
- * 
- * @author Steven J. Metsker
- */
 public abstract class OozinozFilter extends FilterWriter 
 {
 protected OozinozFilter(Writer out)
@@ -47,15 +30,7 @@ public void write(char cbuf[], int off, int len)
  * @throws IOException if an I/O error occurs
  */
 public abstract void write(int c) throws IOException;
-/**
- * Write a portion of a string.
- *
- * @param str String to be written
- * @param off Offset from which to start reading characters
- * @param len Number of characters to be written
- *
- * @throws IOException if an I/O error occurs
- */
+
 public void write(String s, int off, int len)
 throws IOException 
 {
