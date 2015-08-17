de1bc9477a6890903ab537da53f70ddd0b61fce8
Levis Jani <levisjani@rocketmail.com>
2015-08-17 23:31:51 +0200

    Update ComponentIterator.java

--- oozinoz/util/ComponentIterator.java
+++ oozinoz/util/ComponentIterator.java
@@ -2,37 +2,6 @@ package com.oozinoz.util;
 
 import java.util.*;
 
-/*
- * Copyright (c) 2001 Steven J. Metsker.
- * 
- * Steve Metsker makes no representations or warranties about
- * the fitness of this software for any particular purpose, 
- * including the implied warranty of merchantability.
- *
- * Please use this software as you wish with the sole
- * restriction that you may not claim that you wrote it.
- */
- 
-/**
- * This is the abstract superclass of iterators that can
- * walk across leaf nodes and composite nodes in a
- * composite structure.
- * <p>
- * The depth of an iterator is, for the current iteration, 
- * the number of iterators above it. A typical iterator 
- * walks over a composite, iterating over each child. Each 
- * child, however, may also be a composite, so iterating 
- * over a child means iterating over a child's children. In 
- * a tree structure, the root iterator will at any point be 
- * iterating over one of its children. This child will be 
- * iterating over its children. This recurses down to 
- * component that is either a leaf or it a node that has 
- * been visited before. The depth of iteration, then, is 
- * the depth of a subiterator plus one.
- *
- * @author Steven J. Metsker
- *
- */
 public abstract class ComponentIterator 
 {
 
