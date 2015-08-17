ffff5f0796607f9ce3a33d7b0a428b4715119347
Levis Jani <levisjani@rocketmail.com>
2015-08-17 23:31:33 +0200

    Update CommandUtil.java

--- oozinoz/util/CommandUtil.java
+++ oozinoz/util/CommandUtil.java
@@ -1,23 +1,5 @@
 package com.oozinoz.util;
 
-/*
- * Copyright (c) 2001 Steven J. Metsker.
- * 
- * Steve Metsker makes no representations or warranties about
- * the fitness of this software for any particular purpose, 
- * including the implied warranty of merchantability.
- *
- * Please use this software as you wish with the sole
- * restriction that you may not claim that you wrote it.
- */
- 
-/**
- * This class provides a utility for timing the execution
- * of a provided command.
- *
- * @author Steven J. Metsker
- *
- */
 public class CommandUtil 
 {
 /**
